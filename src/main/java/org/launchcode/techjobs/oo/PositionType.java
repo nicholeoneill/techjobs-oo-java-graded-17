c8e34f5b1840dbd4570baac4c1306e581a87eec3
Nichole O'Neill <nazm612@gmail.com>
2023-10-06 09:48:17 -0500

    added constructors, equals and hashcode method, and getters and setters to Job class. Fixed equals and hashcode method in PositionType class

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -26,12 +26,14 @@ public class PositionType {
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return id == that.id && Objects.equals(value, that.value);
+        return id == that.id;
     }
 
     @Override
